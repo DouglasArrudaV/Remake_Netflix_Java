2505c283957a3d27831dccc236d2324044471020
Douglas Arruda Viana <douglas_viana2@hotmail.com>
2022-05-23 16:30:30 -0300

    first commit

---
+++ app/src/main/java/com/example/netflixremake/MovieActivity.java
@@ -0,0 +1,110 @@
+package com.example.netflixremake;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.netflixremake.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieActivity extends AppCompatActivity {
+
+    private TextView txtTitle;
+    private TextView txtDesc;
+    private TextView txtCast;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+
+        txtTitle = findViewById(R.id.text_view_title);
+        txtDesc = findViewById(R.id.text_view_desc);
+        txtCast = findViewById(R.id.text_view_cast);
+        recyclerView = findViewById(R.id.recycler_view_similar);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
+            getSupportActionBar().setTitle(null);
+        }
+
+        LayerDrawable drawable =
+                (LayerDrawable) ContextCompat.getDrawable(this, R.drawable.shadows);
+
+        if (drawable != null) {
+            Drawable movieCover = ContextCompat.getDrawable(this, R.drawable.movie);
+            drawable.setDrawableByLayerId(R.id.cover_drwable, movieCover);
+            ((ImageView) findViewById(R.id.image_view_cover)).setImageDrawable(drawable);
+        }
+
+        txtTitle.setText("Titans");
+        txtDesc.setText("Titans acompanha um grupo de jovens e futuros super-heróis do universo DC. Na série, Dick Grayson (Brenton Thwaites) surge das sombras para se tornar o líder de uma equipe destemida de novos heróis que inclui Estelar (Anna Diop), Raven (Teagan Croft), entre outros.");
+        txtCast.setText(getString(R.string.cast, "Robin" + "estelar" + "dick" + "estrela negra"));
+
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            Movie movie = new Movie();
+            movies.add(movie);
+        }
+
+        recyclerView.setAdapter(new MovieAdapter(movies));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+
+    }
+
+    private static class MovieHolder extends RecyclerView.ViewHolder {
+
+        final ImageView imageViewCover;
+
+        public MovieHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewCover = itemView.findViewById(R.id.image_view_cover);
+        }
+    }
+
+    private class MovieAdapter extends RecyclerView.Adapter<MovieHolder> {
+
+        private final List<Movie> movies;
+
+        private MovieAdapter(List<Movie> movies) {
+            this.movies = movies;
+        }
+
+        @NonNull
+        @Override
+        public MovieHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MovieHolder(getLayoutInflater()
+                    .inflate(R.layout.movie_item_similar, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MovieHolder holder, int position) {
+            Movie movie = movies.get(position);
+            //holder.imageViewCover.setImageResource(movie.getCoverUrl());
+        }
+
+        @Override
+        public int getItemCount() {
+            return movies.size();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/netflixremake/model/Movie.java
@@ -0,0 +1,16 @@
+package com.example.netflixremake.model;
+
+public class Movie {
+
+    private String coverUrl;
+
+    public String getCoverUrl() {
+        return coverUrl;
+    }
+
+    public void setCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
+    }
+
+}
+
